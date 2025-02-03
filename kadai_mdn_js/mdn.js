93002b071f6682aadf3f319c67603acd1b877c2a
nakayama0206 <d1209r@yahoo.co.jp>
2025-02-03 09:29:22 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,6 @@
 const today = new Date();
 
-console.log(today.getFullYear());  // 年を表示
-console.log(today.getMonth() + 1); // 月を表示
-console.log(today.getDate());      // 日付を表示
+const year = today.getFullYear();  // 年を表示
+const month = today.getMonth() + 1; // 月を表示
+const day = today.getDate();      // 日付を表示
+console.log(year + '年' + month + '月' + day + '日');
